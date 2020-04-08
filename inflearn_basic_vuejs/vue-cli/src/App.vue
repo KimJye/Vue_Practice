f80cc48014dd29dee4b62f99c670a4c3f6e4d14a
kimjye <eoeogudgud@naver.com>
2020-04-08 18:04:30 +0900

    feat: 싱글 파일 컴포넌트에서 emit, event 구현

--- inflearn_basic_vuejs/vue-cli/src/App.vue
+++ inflearn_basic_vuejs/vue-cli/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <app-header v-bind:propsdata="str"></app-header>
+    <app-header 
+      v-bind:propsdata="str"
+      v-on:renew="renewStr"
+     ></app-header>
   </div>
 </template>
 
@@ -15,7 +18,12 @@ export default {
   },
   components:{
     'app-header' : AppHeader
-  }
+  },
+  methods: {
+    renewStr:function(){
+      this.str = 'ruby';
+    }
+  },
 }
 </script>
 
