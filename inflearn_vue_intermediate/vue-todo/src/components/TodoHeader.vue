e78b510100450107b9e88fd486ac9d45630b8c50
kimjye <eoeogudgud@naver.com>
2020-04-08 21:32:30 +0900

    feat: 파비콘과 TodoHeader

--- inflearn_vue_intermediate/vue-todo/src/components/TodoHeader.vue
+++ inflearn_vue_intermediate/vue-todo/src/components/TodoHeader.vue
@@ -1,15 +1,13 @@
 <template>
-  <div>
-      Header
-  </div>
+  <header>
+      <h1>TODO it!</h1>
+  </header>
 </template>
 
-<script>
-export default {
-
+<style scoped>
+h1 {
+    color : #2f3b52;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
 }
-</script>
-
-<style>
-
 </style>
