fadf30916639c1bde668fe42712a790ec75e6cb2
kimjye <eoeogudgud@naver.com>
2020-04-08 20:14:35 +0900

    feat: 사용자 입력 폼 예제

---
+++ inflearn_basic_vuejs/vue-form/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <form v-on:submit.prevent="submitForm">
+    <div>
+      <label for="username">id: </label>
+      <input id="username" type="text" v-model="username">
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input id="password" type="password" v-model="password">
+    </div>
+    <button type="submit">
+      login
+    </button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data: function(){
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    submitForm: function(){
+      // event.preventDefault(); // vanila js 방식이다. vue에서는 .prevent하면 됨!!
+      console.log(this.username, this.password);
+      
+      const url = 'https://jsonplaceholder.typicode.com/users';
+
+      const data = {
+        username : this.username,
+        password : this.password
+      }
+
+      axios.post(url, data)
+      .then(res=>{
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    }  
+  }
+}
+</script>
+
+<style>
+
+</style>
