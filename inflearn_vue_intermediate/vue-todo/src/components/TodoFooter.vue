6c5910e2efcea8e29d3606e4a5c77fda1bce870a
kimjye <eoeogudgud@naver.com>
2020-04-09 00:09:30 +0900

    feat: footer

--- inflearn_vue_intermediate/vue-todo/src/components/TodoFooter.vue
+++ inflearn_vue_intermediate/vue-todo/src/components/TodoFooter.vue
@@ -1,15 +1,31 @@
 <template>
-   <div>
-      Footer
+   <div class='clearAllContainer'>
+      <span class='clearAllBtn' v-on:click='clearTodo'>Clear All</span>
   </div>
 </template>
 
 <script>
 export default {
-
+   methods: {
+      clearTodo: function(){
+         localStorage.clear()
+      }
+   },
 }
 </script>
 
-<style>
+<style scoped>
+.clearAllContainer {
+   width: 8.5rem;
+   height: 5rem;
+   line-height: 5rem;
+   background-color: white;
+   border-radius: 0.5rem;
+   margin: 0 auto;
+}
 
+.clearAllBtn {
+   color: #e20303;
+   display: block;
+}
 </style>
