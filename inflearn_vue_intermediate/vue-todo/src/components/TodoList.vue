d3273260852c85a5d10494ce7e5f013f7151f35f
kimjye <eoeogudgud@naver.com>
2020-04-08 23:28:49 +0900

    feat: 할일 삭제 구현

--- inflearn_vue_intermediate/vue-todo/src/components/TodoList.vue
+++ inflearn_vue_intermediate/vue-todo/src/components/TodoList.vue
@@ -1,9 +1,9 @@
 <template>
    <div>
       <ul>
-         <li v-for='todoItem in todoItems' v-bind:key='todoItem' class='shadow'>
+         <li v-for='(todoItem, index) in todoItems' v-bind:key='todoItem' class='shadow'>
             {{todoItem}}
-            <span class='removeBtn' v-on:click="removeTodo">
+            <span class='removeBtn' v-on:click="removeTodo(todoItem, index)">
                <i class='fas fa-trash-alt'></i>
             </span>
          </li>
@@ -29,8 +29,9 @@ export default {
    },
 
    methods: {
-      removeTodo: function(){
-         
+      removeTodo: function(todoItem, index){
+         localStorage.removeItem(todoItem)
+         this.todoItems.splice(index, 1);
       }
    },
 }
