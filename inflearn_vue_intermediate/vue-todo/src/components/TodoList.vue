962036440f2ddf000985421ce934b44e200c05b8
kimjye <eoeogudgud@naver.com>
2020-04-08 23:21:24 +0900

    feat: 디자인

--- inflearn_vue_intermediate/vue-todo/src/components/TodoList.vue
+++ inflearn_vue_intermediate/vue-todo/src/components/TodoList.vue
@@ -1,8 +1,11 @@
 <template>
    <div>
       <ul>
-         <li v-for='todoItem in todoItems' v-bind:key='todoItem'>
+         <li v-for='todoItem in todoItems' v-bind:key='todoItem' class='shadow'>
             {{todoItem}}
+            <span class='removeBtn' v-on:click="removeTodo">
+               <i class='fas fa-trash-alt'></i>
+            </span>
          </li>
       </ul>
   </div>
@@ -23,10 +26,52 @@ export default {
             this.todoItems.push(localStorage.key(i))
          }
       }
-   }
+   },
+
+   methods: {
+      removeTodo: function(){
+         
+      }
+   },
 }
 </script>
 
 <style>
+ul {
+   list-style-type: none;
+   padding-left: 0px;
+   margin-top: 0;
+   text-align: left;
+}
 
+li {
+   display: flex;
+   min-height: 5rem;
+   height: 5rem;
+   line-height: 5rem;
+   margin: 0.5rem 0;
+   padding: 0 0.9rem;
+   background: white;
+   border-radius: 5px;
+}
+
+.checkBtn {
+   line-height: 4.5rem;
+   color: #62acde;
+   margin-right: 0.5rem;
+}
+
+.checkBtnCompleted {
+   color: #b3adad;
+}
+
+.textCompleted {
+   text-decoration: line-through;
+   color: #b3adad;
+}
+
+.removeBtn {
+   margin-left: auto;
+   color: #de4343;
+}
 </style>
