e6043ed8292cba9ac144d159cb0b5e60223ce1cc
kimjye <eoeogudgud@naver.com>
2020-04-09 01:07:20 +0900

    refactor: 할일 모두 삭제

--- inflearn_vue_intermediate/vue-todo/src/App.vue
+++ inflearn_vue_intermediate/vue-todo/src/App.vue
@@ -6,7 +6,7 @@
              v-on:removeItem='removeOneItem' 
              v-on:toggleItem='toggleOneItem'>
     </TodoList>
-    <TodoFooter></TodoFooter>
+    <TodoFooter v-on:clearAll='cleareAllItems'></TodoFooter>
   </div>
 </template>
 
@@ -38,6 +38,10 @@ export default {
       this.todoItems[index].completed = !this.todoItems[index].completed
       localStorage.removeItem(todoItem.item)
       localStorage.setItem(todoItem.item, JSON.stringify(todoItem))
+    },
+    cleareAllItems: function(){
+      localStorage.clear()
+      this.todoItems = []
     }
   },
 
