f447f5a4406ddf6b5227f0b69dab0016810f0cc8
kimjye <eoeogudgud@naver.com>
2020-04-09 23:38:50 +0900

    feat: 모달, slot

--- inflearn_vue_intermediate/vue-todo/src/components/TodoInput.vue
+++ inflearn_vue_intermediate/vue-todo/src/components/TodoInput.vue
@@ -1,17 +1,30 @@
 <template>
    <div class='inputBox shadow'>
-      <input type='text' v-model='newTodoItem' v-on:keyup.enter='addTodo'>
-      <span class='addContainer' v-on:click='addTodo'>
-          <i class='fas fa-plus addBtn'></i>
-      </span>
+    <input type='text' v-model='newTodoItem' v-on:keyup.enter='addTodo'>
+    <span class='addContainer' v-on:click='addTodo'>
+        <i class='fas fa-plus addBtn'></i>
+    </span>
+
+    <Modal v-if='showModal' @close='showModal = false'>
+        <!-- <div class='modal-header'>경고!</div> -->
+        <h3 slot='header'>
+            경고!
+            <i class='closeModalBtn fas fa-times' @click='showModal = false'></i>
+        </h3>
+        <div slot='body'>무언가를 입력하세요</div>
+    </Modal>
   </div>
 </template>
 
 <script>
+
+import Modal from './common/Modal.vue';
+
 export default {
     data:function(){
         return {
-            newTodoItem: ''
+            newTodoItem: '',
+            showModal: false
         }
     },
     methods: {
@@ -19,12 +32,17 @@ export default {
             if(this.newTodoItem !== '') {
                this.$emit('addTodoItem', this.newTodoItem);
                this.clearInput();
+            }else{
+                this.showModal = !this.showModal;
             }
         },
         clearInput: function(){
             this.newTodoItem='';
         }
     },
+    components: {
+        Modal: Modal
+    }
 }
 </script>
 
@@ -56,5 +74,9 @@ input:focus {
 .addBtn {
     color:white;
     vertical-align: middle;
+}
+
+.closeModalBtn {
+    color: #42b983;
 }
 </style>
