fe63af375b68c8de2655ba13b70821e2e4c2dc91
kimjye <eoeogudgud@naver.com>
2020-04-10 00:06:22 +0900

    refactor: es6 메서드 속성 축약

--- inflearn_vue_intermediate/vue-todo/src/App.vue
+++ inflearn_vue_intermediate/vue-todo/src/App.vue
@@ -18,34 +18,34 @@ import TodoFooter from './components/TodoFooter.vue'
 
 export default {
 
-  data: function() {
+  data() {
     return {
         todoItems:[]
     }
   },
 
   methods: {
-    addOneItem: function(todoItem) {
+    addOneItem(todoItem) {
       const obj = {completed: false, item: todoItem};
       localStorage.setItem(todoItem, JSON.stringify(obj));
       this.todoItems.push(obj);
     },
-    removeOneItem: function(todoItem, index){
+    removeOneItem(todoItem, index){
       localStorage.removeItem(todoItem.item)
       this.todoItems.splice(index, 1)
     },
-    toggleOneItem: function(todoItem, index){
+    toggleOneItem(todoItem, index){
       this.todoItems[index].completed = !this.todoItems[index].completed
       localStorage.removeItem(todoItem.item)
       localStorage.setItem(todoItem.item, JSON.stringify(todoItem))
     },
-    cleareAllItems: function(){
+    cleareAllItems(){
       localStorage.clear()
       this.todoItems = []
     }
   },
 
-  created: function(){
+  created(){
     if(localStorage.length > 0) {
         for(let i = 0; i<localStorage.length; ++i){
           if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
--- inflearn_vue_intermediate/vue-todo/src/components/TodoInput.vue
+++ inflearn_vue_intermediate/vue-todo/src/components/TodoInput.vue
@@ -21,14 +21,14 @@
 import Modal from './common/Modal.vue';
 
 export default {
-    data:function(){
+    data(){
         return {
             newTodoItem: '',
             showModal: false
         }
     },
     methods: {
-        addTodo: function(){
+        addTodo(){
             if(this.newTodoItem !== '') {
                this.$emit('addTodoItem', this.newTodoItem);
                this.clearInput();
@@ -36,7 +36,7 @@ export default {
                 this.showModal = !this.showModal;
             }
         },
-        clearInput: function(){
+        clearInput(){
             this.newTodoItem='';
         }
     },
