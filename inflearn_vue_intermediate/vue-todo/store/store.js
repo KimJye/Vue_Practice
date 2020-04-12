472bd094ccaafdbe4164cc3b011a47275a58a396
kimjye <eoeogudgud@naver.com>
2020-04-13 02:45:47 +0900

    refactor: 모듈화

--- inflearn_vue_intermediate/vue-todo/store/store.js
+++ inflearn_vue_intermediate/vue-todo/store/store.js
@@ -1,29 +1,11 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import * as getters from './getters';
-import * as mutations from './mutations';
+import todoApp from './modules/todoApp';
 
 Vue.use((Vuex));
 
-const storage = {
-    fetch() {
-        const arr = [];
-        if (localStorage.length > 0) {
-            for (let i = 0; i < localStorage.length; ++i) {
-                if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
-                    arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-                }
-            }
-        }
-        return arr;
-    }
-}
-
 export const store = new Vuex.Store({
-    state: {
-        todoItems: storage.fetch()
-    },
-    
-    getters,
-    mutations
+    modules: {
+       todoApp
+    }
 });
