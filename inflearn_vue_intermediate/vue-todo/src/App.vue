bc3497c29d767a79dd78cc8ab6461fb6cf925408
kimjye <eoeogudgud@naver.com>
2020-04-13 01:16:50 +0900

    feat: vuex 적용

--- inflearn_vue_intermediate/vue-todo/src/App.vue
+++ inflearn_vue_intermediate/vue-todo/src/App.vue
@@ -1,60 +1,19 @@
 <template>
   <div id='app'>
-    <TodoHeader></TodoHeader>
-    <TodoInput v-on:addTodoItem='addOneItem'></TodoInput>
-    <TodoList v-bind:propsdata='todoItems'
-             v-on:removeItem='removeOneItem' 
-             v-on:toggleItem='toggleOneItem'>
-    </TodoList>
-    <TodoFooter v-on:clearAll='cleareAllItems'></TodoFooter>
+    <TodoHeader/>
+    <TodoInput/>
+    <TodoList/>
+    <TodoFooter/>
   </div>
 </template>
 
 <script>
-import TodoHeader from './components/TodoHeader.vue'
-import TodoInput from './components/TodoInput.vue'
-import TodoList from './components/TodoList.vue'
-import TodoFooter from './components/TodoFooter.vue'
+import TodoHeader from './components/TodoHeader.vue';
+import TodoInput from './components/TodoInput.vue';
+import TodoList from './components/TodoList.vue';
+import TodoFooter from './components/TodoFooter.vue';
 
 export default {
-
-  data() {
-    return {
-        todoItems:[]
-    }
-  },
-
-  methods: {
-    addOneItem(todoItem) {
-      const obj = {completed: false, item: todoItem};
-      localStorage.setItem(todoItem, JSON.stringify(obj));
-      this.todoItems.push(obj);
-    },
-    removeOneItem(todoItem, index){
-      localStorage.removeItem(todoItem.item)
-      this.todoItems.splice(index, 1)
-    },
-    toggleOneItem(todoItem, index){
-      this.todoItems[index].completed = !this.todoItems[index].completed
-      localStorage.removeItem(todoItem.item)
-      localStorage.setItem(todoItem.item, JSON.stringify(todoItem))
-    },
-    cleareAllItems(){
-      localStorage.clear()
-      this.todoItems = []
-    }
-  },
-
-  created(){
-    if(localStorage.length > 0) {
-        for(let i = 0; i<localStorage.length; ++i){
-          if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
-            this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))))
-          }  
-        }
-    }
-  },
-
   components:{
     TodoHeader,
     TodoInput,
--- inflearn_vue_intermediate/vue-todo/src/components/TodoInput.vue
+++ inflearn_vue_intermediate/vue-todo/src/components/TodoInput.vue
@@ -30,7 +30,8 @@ export default {
     methods: {
         addTodo(){
             if(this.newTodoItem !== '') {
-               this.$emit('addTodoItem', this.newTodoItem);
+            const text = this.newTodoItem.trim();
+            this.$store.commit('addOneItem', text);
                this.clearInput();
             }else{
                 this.showModal = !this.showModal;
