aba4d8a3ed8257a3ba72850cd6ae4b0ced7774b0
kimjye <eoeogudgud@naver.com>
2020-04-15 00:04:45 +0900

    feat: 데이터 불러오기

--- inflearn-vue-trello/src/components/Board.vue
+++ inflearn-vue-trello/src/components/Board.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
     Board
-    <div>bid: {{ bid }}</div>
-    <router-link :to="`/board/${bid}/card/1`"> Card 1</router-link>
-    <router-link :to="`/board/${bid}/card/2`"> Card 2</router-link>
+    <div v-if="loading">loading board...</div>
+    <div v-else>
+      <div>bid: {{ bid }}</div>
+      <router-link :to="`/board/${bid}/card/1`"> Card 1</router-link>
+      <router-link :to="`/board/${bid}/card/2`"> Card 2</router-link>
+    </div>
     <hr />
     <router-view></router-view>
   </div>
@@ -14,10 +17,20 @@ export default {
   data() {
     return {
       bid: 0,
+      loading: false,
     };
   },
   created() {
-    this.bid = this.$route.params.bid;
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      this.loading = true;
+      setTimeout(() => {
+        this.bid = this.$route.params.bid;
+        this.loading = false;
+      }, 500);
+    },
   },
 };
 </script>
