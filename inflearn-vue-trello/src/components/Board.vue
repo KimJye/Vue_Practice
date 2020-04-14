5f02df92402ba2325a95a330ecc683aa8e26bebb
kimjye <eoeogudgud@naver.com>
2020-04-14 23:33:27 +0900

    feat: 중첩 라우트

--- inflearn-vue-trello/src/components/Board.vue
+++ inflearn-vue-trello/src/components/Board.vue
@@ -2,6 +2,10 @@
   <div>
     Board
     <div>bid: {{ bid }}</div>
+    <router-link :to="`/board/${bid}/card/1`"> Card 1</router-link>
+    <router-link :to="`/board/${bid}/card/2`"> Card 2</router-link>
+    <hr />
+    <router-view></router-view>
   </div>
 </template>
 
