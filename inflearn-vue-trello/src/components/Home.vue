272f7f9653838966459d92e5242969c34582b507
kimjye <eoeogudgud@naver.com>
2020-04-14 23:18:24 +0900

    feat: 동적 라우팅 매칭

--- inflearn-vue-trello/src/components/Home.vue
+++ inflearn-vue-trello/src/components/Home.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>HOME</div>
+  <div>
+    HOME
+    <div>
+      Boad List:
+      <ul>
+        <li>
+          <router-link to="board/1">Board 1</router-link>
+        </li>
+        <li>
+          <router-link to="board/2">Board 2</router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
